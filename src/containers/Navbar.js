20a86bc8a83a5d06734a3e1e4a9ce3edb36a6308
Ash Zaki <ashleya.zaki@gmail.com>
2019-04-24 23:24:43 -0400

    added semantic UI

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,17 +1,47 @@
 import React, {Component, Fragment} from 'react'
+import {  Menu, Segment } from 'semantic-ui-react'
 import RoomCreate from '../components/RoomCreate'
 import UserLogin from '../components/UserLogin'
 import UserLogout from '../components/UserLogout'
 import Register from '../components/Register'
 
 class Navbar extends Component {
+
+    state = {
+        activeItem: 'home'
+    }
+
+    handleItemClick = (e, { name }) => {
+        this.setState({ 
+            activeItem: name 
+        })
+    } 
+
     render(){
+        const { activeItem } = this.state
+
         return (
             <Fragment>
-                <RoomCreate />
-                <UserLogin />
-                <UserLogout />
-                <Register />
+                <Menu pointing secondary>
+                    <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                    <Menu.Item
+                        name='messages'
+                        active={activeItem === 'messages'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='friends'
+                        active={activeItem === 'friends'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Menu position='right'>
+                        <Menu.Item
+                        name='logout'
+                        active={activeItem === 'logout'}
+                        onClick={this.handleItemClick}
+                        />
+                    </Menu.Menu>
+                </Menu>
             </Fragment>
         )
     }
